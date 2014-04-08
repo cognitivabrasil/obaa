89c06d1213327dd5ce7cb5c2f79116e3620cce6e
luiz <luiz@Cognitiva-02>
2014-04-08 16:24:32 -0300

    FEB-391 Criado um parser para o format, para pegar o tipo tem que dar um getCategory()

--- src/main/java/cognitivabrasil/obaa/Technical/Format.java
+++ src/main/java/cognitivabrasil/obaa/Technical/Format.java
@@ -1,13 +1,16 @@
-/*******************************************************************************
- * Copyright (c) 2013 Cognitiva Brasil - Tecnologias educacionais.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v3
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
- ******************************************************************************/
+/**
+ * *****************************************************************************
+ * Copyright (c) 2013 Cognitiva Brasil - Tecnologias educacionais. All rights
+ * reserved. This program and the accompanying materials are made available
+ * under the terms of the GNU Lesser Public License v3 which accompanies this
+ * distribution, and is available at http://www.gnu.org/licenses/lgpl.html
+ * ****************************************************************************
+ */
 package cognitivabrasil.obaa.Technical;
 
+import cognitivabrasil.util.MimeType;
 import metadata.TextElement;
+import org.apache.log4j.Logger;
 import org.simpleframework.xml.Namespace;
 
 /**
@@ -19,11 +22,75 @@ import org.simpleframework.xml.Namespace;
 @Namespace(reference = "http://ltsc.ieee.org/xsd/LOM", prefix = "obaa")
 public class Format extends TextElement {
 
+    MimeType mimeType;
+    private static final Logger log = Logger.getLogger(Format.class);
+    String category;
+
     public Format() {
         super();
+        mimeType = new MimeType();
     }
 
     public Format(String t) {
         super(t);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    @Override
+    public void setText(String format) {
+
+//        if (format.equals("jpg")) {
+//            System.out.println("!!!!!");
+//        }
+
+        format = format.toLowerCase();
+        format = format.replaceAll("imagem", "image");
+        
+        String mime = mimeType.getMimeType(format);
+        if (mime == null) {
+            log.warn("Mime Type não identificado.");
+
+            if (format.matches(".*\\[?\\d+\\]?\\s*(f|p|v)\\.\\s*.*")) {
+                this.setCategory("text");
+                super.setText("application/pdf");                
+                
+            } else if (format.contains("flash")){
+                this.setCategory("application");
+                super.setText("application/x-shockwave-flash");                
+            }
+            else {                
+                super.setText(format);
+                log.warn("Não foi possível interpretar o mime type.");
+                
+                if (format.contains("video")){
+                    this.setCategory("video");
+                } 
+                else if (format.contains("image")||format.contains("imagem")) {
+                    this.setCategory("image");
+                }
+                  else if (format.contains("text")||format.contains("texto")||format.contains("descritivo")) {
+                    this.setCategory("text");
+                }
+                  else if (format.contains("application")||format.contains("aplicação")||format.contains("swf")
+                          ||format.contains("fla")||format.contains("scorm")||format.contains("javascript")) {
+                    this.setCategory("application");
+                }  
+                  else if (format.contains("audio")) {
+                    this.setCategory("audio");
+                }   
+            }
+
+        } else {
+            this.setCategory(mime.substring(0, mime.indexOf("/")));
+            super.setText(mime);
+        }
+        
     }
 }
