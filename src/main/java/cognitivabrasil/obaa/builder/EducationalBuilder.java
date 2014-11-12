805f568671682e32ed37eb968be5cfa101697077
Paulo Schreiner <paulo.schreiner@gmail.com>
2014-11-12 16:13:55 -0200

    Adicionei arquivo que faltou.

---
+++ src/main/java/cognitivabrasil/obaa/builder/EducationalBuilder.java
@@ -0,0 +1,39 @@
+package cognitivabrasil.obaa.builder;
+
+
+import cognitivabrasil.obaa.OBAA;
+import cognitivabrasil.obaa.Educational.Educational;
+
+public class EducationalBuilder extends AbstractObaaBuilder {
+  
+    public EducationalBuilder(OBAA obaa) {
+        super(obaa);
+        getObaa().setEducational(new Educational());
+    }
+
+    public EducationalBuilder interactivityType(String string) {
+        getObaa().getEducational().setInteractivityType(string);
+        
+        return this;
+    }
+
+    public EducationalBuilder learningResourceType(String string) {
+        getObaa().getEducational().addLearningResourceType(string);
+        
+        return this;
+    }
+
+    public EducationalBuilder intendedEndUserRole(String string) {
+        getObaa().getEducational().addIntendedEndUserRole(string);
+        
+        return this;        
+    }
+
+    public EducationalBuilder context(String string) {
+        getObaa().getEducational().addContext(string);
+        
+        return this;
+    }
+
+
+}
